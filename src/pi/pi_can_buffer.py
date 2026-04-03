6e0aaf565c47f844a8b2ed08d746335a6c421b71
majockbim <majock.bim1@gmail.com>
2026-04-03 00:56:30 -0400

    create queue logic, constrained by 187 values

--- src/pi/pi_can_buffer.py
+++ src/pi/pi_can_buffer.py
@@ -7,7 +7,7 @@ target = 187 # // request by dilly
 # format: IMU#_GYRO/ACCEL_ID
 imu1_a_id = 0x123
 imu1_g_id = 0x124
-'''
+''' for the future
 imu2_a_id = 0x125
 imu2_g_id = 0x126
 
@@ -18,7 +18,7 @@ imu4_a_id = 0x129
 imu4_g_id = 0x130
 '''
 
-# // hold state (queue, time) for each IMU
+# // hold state (accumulator, queue) for each IMU. queue has ('time', '(x,y,z)')
 can_streams = {
     imu1_a_id: {'accumulator': 0, 'queue': deque(maxlen=target)},
     imu1_g_id: {'accumulator': 0, 'queue': deque(maxlen=target)}
@@ -29,15 +29,32 @@ bus = can.Bus(interface='socketcan', channel='can1')
 
 print("buffering joint data...")
 
+def process_msg(msg: can.Message):
+    can_id = msg.arbitration_id
 
-'''
-while True:
-    msg = bus.recv()
-    
-    if msg.arbitration_id == 0x123:
-        ax = int.from_bytes(msg.data[1:3], byteorder='little', signed=True)
-        ay = int.from_bytes(msg.data[3:5], byteorder='little', signed=True)
-        az = int.from_bytes(msg.data[5:7], byteorder='little', signed=True)
+    # // if the id is something like the power readings for the IMU, ignore
+    if can_id not in can_streams:
+        return
+
+    # // get accumulator, and queue for the ID -> accumulate
+    stream_state = can_streams[can_id]
+    stream_state['accumulator'] += target
+
+    if stream_state['accumulator'] >= hz:
+        try:
+            x_raw, y_raw, z_raw = struct.unpack('<hhh', msg.data[0:6])
+
+            x = x_raw / 100.0
+            y = y_raw / 100.0
+            z = z_raw / 100.0
+
+            # // mettre la donnees dans le file d'attente (+horodatage)
+            data_tuple = (msg.timestamp, (x, y, z))
+
+            stream_state['queue'].append(data_tuple)
         
-        accel_history.append((ax, ay, az))
-'''
+        except Exception as e:
+            print("Oh no, cannot unpack")
+        
+        # // reset accumulator
+        stream_state['accumulator'] -= hz
