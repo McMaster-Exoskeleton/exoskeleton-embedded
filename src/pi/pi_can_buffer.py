287191d754a5e0c4bcc4a58b7b7c1b917cd59059
majockbim <majock.bim1@gmail.com>
2026-04-03 00:42:10 -0400

    refactor/Change up the buffer (initial)

--- src/pi/pi_can_buffer.py
+++ src/pi/pi_can_buffer.py
@@ -1,13 +1,36 @@
 import can
-import collections
+from collections import deque
 
-accel_history = collections.deque(maxlen=500)
-gyro_history = collections.deque(maxlen=500)
+hz = 500
+target = 187 # // request by dilly
+
+# format: IMU#_GYRO/ACCEL_ID
+imu1_a_id = 0x123
+imu1_g_id = 0x124
+'''
+imu2_a_id = 0x125
+imu2_g_id = 0x126
+
+imu3_a_id = 0x127
+imu3_g_id = 0x128
+
+imu4_a_id = 0x129
+imu4_g_id = 0x130
+'''
+
+# // hold state (queue, time) for each IMU
+can_streams = {
+    imu1_a_id: {'accumulator': 0, 'queue': deque(maxlen=target)},
+    imu1_g_id: {'accumulator': 0, 'queue': deque(maxlen=target)}
+    # add rest
+}
 
 bus = can.Bus(interface='socketcan', channel='can1')
 
-print("Buffering IMU data...")
+print("buffering joint data...")
 
+
+'''
 while True:
     msg = bus.recv()
     
@@ -17,3 +40,4 @@ while True:
         az = int.from_bytes(msg.data[5:7], byteorder='little', signed=True)
         
         accel_history.append((ax, ay, az))
+'''
